caa4dcde2283ab30097a87814d701742f6521bf2
Vijaya Aditya <vatadepalli@hotmail.com>
2019-12-15 00:11:21 +0530

    externalise env variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularAgain
+# IoTx-Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.14.
 
--- src/app/services/device.service.ts
+++ src/app/services/device.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Device } from "../models/Device";
+import { environment } from "../../environments/environment";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -13,7 +14,7 @@ const httpOptions = {
   providedIn: "root"
 })
 export class DeviceService {
-  deviceUrl: string = "http://localhost:8080";
+  deviceUrl: string = environment.backendurl;
 
   constructor(private http: HttpClient) {}
 
