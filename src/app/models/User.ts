6fe4c5a7f2339107b337fab1bb8a57ef873dd579
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-12 03:55:07 +0530

    css cleanup

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,11 +2,7 @@ export interface User {
   firstName: string;
   lastName: string;
   age?: number;
-  address: {
-    street: string;
-    city: string;
-    state: string;
-  };
+  address: {street: string; city: string; state: string;};
   image?: string;
   isActive?: boolean;
   balance?: number;
